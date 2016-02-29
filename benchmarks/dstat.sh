e209e10fb7f1dee4a5e3069ad3be64fc13a0b7a3
Ricardo Gonçalves <tome.wave@gmail.com>
2016-02-29 19:18:59 +0000

    Fix dstat script.

--- benchmarks/dstat.sh
+++ benchmarks/dstat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dstat --output /home/gsd/BasicDB/_build/default/rel/basic_db/data/stats/current/dstat.csv -talms 1 2400
+dstat --output /home/gsd/BasicDB/benchmarks/tests/dstat.csv -talms 1 2400
